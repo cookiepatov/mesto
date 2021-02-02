88682b7a54b58f4ccf9f52350e6c4b3581566604
cookiepatov <cookiepatov@gmail.com>
2021-02-02 17:42:13 +0300

    переделать обработку попапов и логику добавления карточек

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,17 @@
+import initialCards from './data/cards-data.js';
+
+const errorImgUrl = 'https://images.unsplash.com/photo-1504930268766-d71549a36ec2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1307&q=80';
+
+const popups = document.querySelectorAll('.popup');
+const closeBtns = document.querySelectorAll('.popup__close-button');
+
 const popupProfile = document.querySelector('.popup_type_profile');
 const popupProfileForm = popupProfile.querySelector('.popup__form');
-const closeProfileFormBtn = popupProfile.querySelector('.popup__close-button');
 
 const popupCard = document.querySelector('.popup_type_card');
 const popupCardForm = popupCard.querySelector('.popup__form');
-const closeCardFormBtn = popupCard.querySelector('.popup__close-button');
 
 const popupFullView = document.querySelector('.popup_type_full-view');
-const closeFullViewBtn = popupFullView.querySelector('.popup__close-button');
-
 const openFormBtn = document.querySelector('.profile__edit-button');
 const addCardBtn = document.querySelector('.profile__add-button');
 
@@ -16,36 +19,10 @@ const body = document.querySelector('.body');
 const cardTemplate = document.querySelector('#card-template');
 const elements = document.querySelector('.elements');
 
-const initialCards = [
-  {
-    name: 'Борисово',
-    link: 'https://sun9-52.userapi.com/impg/CL3PVuFGYb1SEWs85Re0TLiMKp7ONGFwp9PaYg/-nplw3ZPS5c.jpg?size=2000x700&quality=96&proxy=1&sign=3887ec7f9a0901fe5089fed815879ea9&type=album'
-  },
-  {
-    name: 'Дендропарк в Бирюлёво',
-    link: 'https://sun9-9.userapi.com/impg/QyyGgUhwIPVWHJSJgVZT0qDx_8--wCqwh3Y6RA/mfmSnV25M7Y.jpg?size=1000x700&quality=96&proxy=1&sign=491d30ca0baba306850eb4e222c87e3c&type=album'
-  },
-  {
-    name: 'Калининград',
-    link: 'https://sun9-65.userapi.com/impg/DVh2ArDKrjyUr3_r2RPoYeY_vHLfcJVfxSlm8g/tha0Z_Fqbpo.jpg?size=2560x1638&quality=96&proxy=1&sign=cdf893318879ed381cc4c04df6a4b4d1&type=album'
-  },
-  {
-    name: 'Котор',
-    link: 'https://sun9-56.userapi.com/impg/byA7uFe7FJNv0vMAk2q0TsVgzNZuYxmIq-JAGQ/nfA3p24XFrw.jpg?size=2560x1587&quality=96&proxy=1&sign=bd8d87536a6465c7d8ccbc2d85958d3a&type=album'
-  },
-  {
-    name: 'Суздаль',
-    link: 'https://sun9-61.userapi.com/impg/-vfiNXrhNjbRW1ksbmdFjSZI_Y04clULTFhdJg/3gwIWQWQp2o.jpg?size=2560x1681&quality=96&proxy=1&sign=53aefbad5f2c58098ffb61940cdc7a6a&type=album'
-  },
-  {
-    name: 'Тиват',
-    link: 'https://sun9-67.userapi.com/impg/H_Flnzr-gxgd2Z3Lor5kQdXH7NU2T9_Xd2L_vg/RQFkkCHugd8.jpg?size=2560x1535&quality=96&proxy=1&sign=c8ad7af02d0a55a0b52c260c2dfeb468&type=album'
-  }
-];
-
 function renderInitialCards(array) {
   array.forEach(element=>{
-    addCard(element.link, element.name)
+    const card = createCard(element.link, element.name);
+    addCard(card);
   })
   //Ниже считаю ширину скроллбара, чтобы корректно высчитывать высоту элементов относительно ширины экрана в vw.
   document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
@@ -77,14 +54,12 @@ function overlayClick(e) {
   }
 }
 
-function errorHandler(e)
-{
+function errorHandler(e) {
   console.log('Неккоректный url: ' + e.target.src);
-  e.target.src='https://images.unsplash.com/photo-1504930268766-d71549a36ec2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1307&q=80';
-
+  e.target.src = errorImgUrl;
 }
 
-function addCard(link, name) {
+function createCard(link, name) {
   const newCard = cardTemplate.content.querySelector('.element').cloneNode(true);
   const pic = newCard.querySelector('.element__picture');
   pic.src=link;
@@ -97,83 +72,77 @@ function addCard(link, name) {
     e.target.closest('.element').remove();
   });
   newCard.querySelector('.element__picture-button').addEventListener('click', function(){
-    openFullView(link, name);
-  })
-  elements.prepend(newCard);
+    openPopup(popupFullView, link, name);
+  });
+  return newCard;
 }
 
-function openProfileForm() {
-  if (popupProfile.classList.contains('popup_opened')) {
-    return;
-  }
-  else {
-    renderProfileFormInfo()
-    popupProfile.classList.add('popup_opened');
-    body.classList.add('no-scroll');
-  }
+function addCard(card) {
+  elements.prepend(card);
 }
 
 function submitProfileForm(e) {
   e.preventDefault();
   profile.name.innerText = formProfile.name.value;
   profile.description.innerText = formProfile.description.value;
-  closeProfileForm();
+  closePopup(e);
 }
 
 function closePopup(e) {
   const popup=e.target.closest('.popup');
-  if (popup.classList.contains('popup_opened'))
-  {
-    popup.classList.remove('popup_opened');
-    body.classList.remove('no-scroll');
-  }
-}
-
-function openCardForm() {
-  if (popupCard.classList.contains('popup_opened')) {
-    return;
-  }
-  else {
-    popupCardForm.reset();
-    popupCard.classList.add('popup_opened');
-    body.classList.add('no-scroll');
-  }
+  popup.classList.remove('popup_opened');
+  body.classList.remove('no-scroll');
 }
 
 function submitCardForm(e) {
   e.preventDefault();
   const name = formNewCard.name.value;
   const link = formNewCard.data.value;
-  addCard(link, name);
+  const card = createCard(link, name);
+  addCard(card);
   closePopup(e);
 }
 
-
-function openFullView(link, name) {
+function openPopup(popup, link='', name='') {
   body.classList.add('no-scroll');
+  popup.classList.add('popup_opened');
+  switch (popup) {
+    case popupProfile:
+      renderProfileFormInfo();
+      break;
+    case popupCard:
+      resetCardPopup();
+      break;
+    case popupFullView:
+      renderFullView(link, name);
+      break;
+  }
+}
+
+function resetCardPopup() {
+  popupCardForm.reset();
+}
+
+function renderFullView(link, name) {
   const pic = popupFullView.querySelector('.popup__full-picture');
-  pic.src=link;
-  pic.alt=name;
+  pic.src = link;
+  pic.alt = name;
   pic.onerror = errorHandler;
-  popupFullView.querySelector('.popup__picture-title').textContent=name;
-  popupFullView.classList.add('popup_opened');
+  popupFullView.querySelector('.popup__picture-title').textContent = name;
 }
 
 function init () {
   renderInitialCards(initialCards);
 
-  openFormBtn.addEventListener('click', openProfileForm);
-  closeProfileFormBtn.addEventListener('click', closePopup);
+  popups.forEach((popup) => popup.addEventListener('pointerdown',overlayClick));
+  closeBtns.forEach((btn) => btn.addEventListener('click', closePopup));
+
+  openFormBtn.addEventListener('click',()=>{openPopup(popupProfile)});
   popupProfileForm.addEventListener('submit', submitProfileForm);
-  popupProfile.addEventListener('click', overlayClick);
 
-  addCardBtn.addEventListener('click', openCardForm);
-  closeCardFormBtn.addEventListener('click', closePopup);
+  addCardBtn.addEventListener('click',()=>openPopup(popupCard));
   popupCardForm.addEventListener('submit', submitCardForm);
-  popupCard.addEventListener('click', overlayClick);
 
-  closeFullViewBtn.addEventListener('click', closePopup);
-  popupFullView.addEventListener('click', overlayClick);
 }
 
 init();

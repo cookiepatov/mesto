318bf5b1b60ee441747abe350edb914318260eda
cookiepatov <cookiepatov@gmail.com>
2021-04-08 03:16:24 +0300

    Оптимизировать _resultHandler

--- src/components/Api.js
+++ src/components/Api.js
@@ -6,10 +6,7 @@ export default class Api {
   }
 
   _resultHandler(res) {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Ошибка: ${res.status}`);
+    return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
   }
 
 
